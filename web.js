93f564a7844aafc83d83009ca3bd58c9da7da6a9
Tobias Wirén <tobias.wiren@gmail.com>
2013-07-07 00:05:33 +0200

    serve index.html

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var indexHtml = fs.readFileSync('index.html');
+    response.send(indexHtml.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
